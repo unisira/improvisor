3df330d0a3539bbaff8ef5d062480c7f283e8d72
unisira <owenmc362@gmail.com>
2021-06-29 19:23:10 +0100

    Finished implementing the intrinsics for CPU state saving & restoration

--- src/cpu_state.asm
+++ src/cpu_state.asm
@@ -5,7 +5,6 @@ _Rax QWORD ?
 _Rbx QWORD ?
 _Rcx QWORD ?
 _Rdx QWORD ?
-_Rbp QWORD ?
 _Rsp QWORD ?
 _Rsi QWORD ?
 _Rdi QWORD ?
@@ -45,22 +44,81 @@ __cpu_save_state PROC
     mov [rcx].CPU_STATE._Rax, rax
     mov [rcx].CPU_STATE._Rbx, rbx
     mov [rcx].CPU_STATE._Rcx, rcx
-    mov [rcx].CPU_STATE._Rdx, rdx,
-    mov [rcx].CPU_STATE._Rdx, rdx,
-    mov [rcx].CPU_STATE._R8, r8,
-    mov [rcx].CPU_STATE._R9, r9,
-    mov [rcx].CPU_STATE._R10, r10,
-    mov [rcx].CPU_STATE._R11, r11,
-    mov [rcx].CPU_STATE._R12, r12,
-    mov [rcx].CPU_STATE._R13, r13,
-    mov [rcx].CPU_STATE._R14, r14,
-    mov [rcx].CPU_STATE._R15, r15,
-    mov [rcx].CPU_STATE._Rbp, rbp,
+    mov [rcx].CPU_STATE._Rdx, rdx
+    mov [rcx].CPU_STATE._Rdx, rsi
+    mov [rcx].CPU_STATE._Rdx, rdi
+    mov [rcx].CPU_STATE._R8, r8
+    mov [rcx].CPU_STATE._R9, r9
+    mov [rcx].CPU_STATE._R10, r10
+    mov [rcx].CPU_STATE._R11, r11
+    mov [rcx].CPU_STATE._R12, r12
+    mov [rcx].CPU_STATE._R13, r13
+    mov [rcx].CPU_STATE._R14, r14
+    mov [rcx].CPU_STATE._R15, r15
+    mov [rcx].CPU_STATE._Rbp, rbp
+    movaps [rcx].CPU_STATE._Xmm0, xmm0
+    movaps [rcx].CPU_STATE._Xmm1, xmm1
+    movaps [rcx].CPU_STATE._Xmm2, xmm2
+    movaps [rcx].CPU_STATE._Xmm3, xmm3
+    movaps [rcx].CPU_STATE._Xmm4, xmm4
+    movaps [rcx].CPU_STATE._Xmm5, xmm5
+    movaps [rcx].CPU_STATE._Xmm6, xmm6
+    movaps [rcx].CPU_STATE._Xmm7, xmm7
+    movaps [rcx].CPU_STATE._Xmm8, xmm8
+    movaps [rcx].CPU_STATE._Xmm9, xmm9
+    movaps [rcx].CPU_STATE._Xmm10, xmm10
+    movaps [rcx].CPU_STATE._Xmm11, xmm11
+    movaps [rcx].CPU_STATE._Xmm12, xmm12
+    movaps [rcx].CPU_STATE._Xmm13, xmm13
+    movaps [rcx].CPU_STATE._Xmm14, xmm14
+    movaps [rcx].CPU_STATE._Xmm15, xmm15
+    stmxcsr [rcx].CPU_STATE._MxCsr
     lea rax, [rsp+8]
-    mov [rcx].CPU_STATE._Rsp, rax,
-    mov rax, [rax]
+    mov [rcx].CPU_STATE._Rsp, rax
+    mov rax, [rsp+8]
     mov [rcx].CPU_STATE._Rip, rax
     mov rax, [rcx].CPU_STATE._Rax
     pop [rcx].CPU_STATE._RFlags
     ret
 __cpu_save_state ENDP
+
+__cpu_restore_state PROC
+    push [rcx].CPU_STATE._RFlags
+    mov rbx, [rcx].CPU_STATE._Rbx
+    mov rcx, [rcx].CPU_STATE._Rcx
+    mov rdx, [rcx].CPU_STATE._Rdx
+    mov rsi, [rcx].CPU_STATE._Rsi
+    mov rdi, [rcx].CPU_STATE._Rdi
+    mov r8, [rcx].CPU_STATE._R8
+    mov r9, [rcx].CPU_STATE._R9
+    mov r10, [rcx].CPU_STATE._R10
+    mov r11, [rcx].CPU_STATE._R11
+    mov r12, [rcx].CPU_STATE._R12
+    mov r13, [rcx].CPU_STATE._R13
+    mov r14, [rcx].CPU_STATE._R14
+    mov r15, [rcx].CPU_STATE._R15
+    mov rbp, [rcx].CPU_STATE._Rbp
+    movaps xmm0, [rcx].CPU_STATE._Xmm0
+    movaps xmm1, [rcx].CPU_STATE._Xmm1
+    movaps xmm2, [rcx].CPU_STATE._Xmm2
+    movaps xmm3, [rcx].CPU_STATE._Xmm3
+    movaps xmm4, [rcx].CPU_STATE._Xmm4
+    movaps xmm5, [rcx].CPU_STATE._Xmm5
+    movaps xmm6, [rcx].CPU_STATE._Xmm6
+    movaps xmm7, [rcx].CPU_STATE._Xmm7
+    movaps xmm8, [rcx].CPU_STATE._Xmm8
+    movaps xmm9, [rcx].CPU_STATE._Xmm9
+    movaps xmm10, [rcx].CPU_STATE._Xmm10
+    movaps xmm11, [rcx].CPU_STATE._Xmm11
+    movaps xmm12, [rcx].CPU_STATE._Xmm12
+    movaps xmm13, [rcx].CPU_STATE._Xmm13
+    movaps xmm14, [rcx].CPU_STATE._Xmm14
+    movaps xmm15, [rcx].CPU_STATE._Xmm15
+    ldmxcsr [rcx].CPU_STATE._MxCsr
+    popfq
+    mov rsp, [rcx].CPU_STATE._Rsp
+    mov rax, [rcx].CPU_STATE._Rip
+    mov [rsp], rax
+    mov rax, [rcx].CPU_STATE._Rax 
+    ret
+__cpu_restore_state ENDP
