414a9a9781a76df8b8ac6b204e01616a90dacce7
unisira <owenmc362@gmail.com>
2021-07-05 16:48:16 +0100

    Add new fields to VMM for memory management and the host IDT

--- src/vmm.h
+++ src/vmm.h
@@ -3,12 +3,16 @@
 
 #include <wdm.h>
 
+#include "arch/segment.h"
+#include "mm/page.h"
 #include "vcpu.h"
 
 typedef struct _VMM_CONTEXT
 {
     UINT8 CpuCount;
     PVCPU VcpuTable;
+    PMM_SUPPORT MmSupport;
+    X86_SYSTEM_DESCRIPTOR HostInterruptDescriptor;
 } VMM_CONTEXT, * PVMM_CONTEXT;
 
 NTSTATUS 
