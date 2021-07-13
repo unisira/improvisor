4159a70554c3d48c8e60a52cf319d2a2c1a7a7b7
unisira <physkotic@gmail.com>
2021-07-13 02:06:26 +0100

    Several fixes to the VM-exit entry point

--- src/vmexit.asm
+++ src/vmexit.asm
@@ -17,8 +17,6 @@ _R14 QWORD ?
 _R15 QWORD ?
 _Rip QWORD ?
 _MxCsr DWORD ?
-_Align1 DWORD ?
-_Align2 QWORD ?
 _Xmm0 XMMWORD ?
 _Xmm1 XMMWORD ?
 _Xmm2 XMMWORD ?
@@ -55,35 +53,32 @@ __vmexit_entry PROC
 	mov [rsp].GUEST_STATE._R13, r13
 	mov [rsp].GUEST_STATE._R14, r14
 	mov [rsp].GUEST_STATE._R15, r15
-	movaps [rsp].GUEST_STATE._Xmm0, xmm0
-	movaps [rsp].GUEST_STATE._Xmm1, xmm1
-	movaps [rsp].GUEST_STATE._Xmm2, xmm2
-	movaps [rsp].GUEST_STATE._Xmm3, xmm3
-	movaps [rsp].GUEST_STATE._Xmm4, xmm4
-	movaps [rsp].GUEST_STATE._Xmm5, xmm5
-	movaps [rsp].GUEST_STATE._Xmm6, xmm6
-	movaps [rsp].GUEST_STATE._Xmm7, xmm7
-	movaps [rsp].GUEST_STATE._Xmm8, xmm8
-	movaps [rsp].GUEST_STATE._Xmm9, xmm9
-	movaps [rsp].GUEST_STATE._Xmm10, xmm10
-	movaps [rsp].GUEST_STATE._Xmm11, xmm11
-	movaps [rsp].GUEST_STATE._Xmm12, xmm12
-	movaps [rsp].GUEST_STATE._Xmm13, xmm13
-	movaps [rsp].GUEST_STATE._Xmm14, xmm14
-	movaps [rsp].GUEST_STATE._Xmm15, xmm15
-
-	mov rax, [rsp]
-	mov [rsp].GUEST_STATE._Rip, rax
+	movups [rsp].GUEST_STATE._Xmm0, xmm0
+	movups [rsp].GUEST_STATE._Xmm1, xmm1
+	movups [rsp].GUEST_STATE._Xmm2, xmm2
+	movups [rsp].GUEST_STATE._Xmm3, xmm3
+	movups [rsp].GUEST_STATE._Xmm4, xmm4
+	movups [rsp].GUEST_STATE._Xmm5, xmm5
+	movups [rsp].GUEST_STATE._Xmm6, xmm6
+	movups [rsp].GUEST_STATE._Xmm7, xmm7
+	movups [rsp].GUEST_STATE._Xmm8, xmm8
+	movups [rsp].GUEST_STATE._Xmm9, xmm9
+	movups [rsp].GUEST_STATE._Xmm10, xmm10
+	movups [rsp].GUEST_STATE._Xmm11, xmm11
+	movups [rsp].GUEST_STATE._Xmm12, xmm12
+	movups [rsp].GUEST_STATE._Xmm13, xmm13
+	movups [rsp].GUEST_STATE._Xmm14, xmm14
+	movups [rsp].GUEST_STATE._Xmm15, xmm15
 	stmxcsr [rsp].GUEST_STATE._MxCsr
 
-	lea rcx, [rsp+SIZEOF GUEST_STATE-6000h]	; Load the address of the stack cache (PVCPU) into RCX
+	mov rcx, [rsp+SIZEOF GUEST_STATE-6000h]	; Load the address of the stack cache (PVCPU) into RCX
 	lea rdx, [rsp] 							; Load the address of the GUEST_STATE into RDX
 	sub rsp, 28h							; Allocate shadow stack space
 	call VcpuHandleExit						; Call the VM-exit handler
 	add rsp, 28h							; Remove shadow stack space
 	
-	ldmxcsr [rsp].GUEST_STATE._MxCsr
-	mov rax, [rsp].GUEST_STATE._Rip			; Restore guest system state
+	ldmxcsr [rsp].GUEST_STATE._MxCsr		; Restore register state
+	mov rax, [rsp].GUEST_STATE._Rip	
 	mov [rsp+SIZEOF GUEST_STATE], rax
 	mov rax, [rsp].GUEST_STATE._Rax			
 	mov rbx, [rsp].GUEST_STATE._Rbx
@@ -99,24 +94,24 @@ __vmexit_entry PROC
 	mov r13, [rsp].GUEST_STATE._R13
 	mov r14, [rsp].GUEST_STATE._R14
 	mov r15, [rsp].GUEST_STATE._R15
-	movaps xmm0, [rsp].GUEST_STATE._Xmm0
-	movaps xmm1, [rsp].GUEST_STATE._Xmm1
-	movaps xmm2, [rsp].GUEST_STATE._Xmm2
-	movaps xmm3, [rsp].GUEST_STATE._Xmm3
-	movaps xmm4, [rsp].GUEST_STATE._Xmm4
-	movaps xmm5, [rsp].GUEST_STATE._Xmm5
-	movaps xmm6, [rsp].GUEST_STATE._Xmm6
-	movaps xmm7, [rsp].GUEST_STATE._Xmm7
-	movaps xmm8, [rsp].GUEST_STATE._Xmm8
-	movaps xmm9, [rsp].GUEST_STATE._Xmm9
-	movaps xmm10, [rsp].GUEST_STATE._Xmm10
-	movaps xmm11, [rsp].GUEST_STATE._Xmm11
-	movaps xmm12, [rsp].GUEST_STATE._Xmm12
-	movaps xmm13, [rsp].GUEST_STATE._Xmm13
-	movaps xmm14, [rsp].GUEST_STATE._Xmm14
-	movaps xmm15, [rsp].GUEST_STATE._Xmm15
+	movups xmm0, [rsp].GUEST_STATE._Xmm0
+	movups xmm1, [rsp].GUEST_STATE._Xmm1
+	movups xmm2, [rsp].GUEST_STATE._Xmm2
+	movups xmm3, [rsp].GUEST_STATE._Xmm3
+	movups xmm4, [rsp].GUEST_STATE._Xmm4
+	movups xmm5, [rsp].GUEST_STATE._Xmm5
+	movups xmm6, [rsp].GUEST_STATE._Xmm6
+	movups xmm7, [rsp].GUEST_STATE._Xmm7
+	movups xmm8, [rsp].GUEST_STATE._Xmm8
+	movups xmm9, [rsp].GUEST_STATE._Xmm9
+	movups xmm10, [rsp].GUEST_STATE._Xmm10
+	movups xmm11, [rsp].GUEST_STATE._Xmm11
+	movups xmm12, [rsp].GUEST_STATE._Xmm12
+	movups xmm13, [rsp].GUEST_STATE._Xmm13
+	movups xmm14, [rsp].GUEST_STATE._Xmm14
+	movups xmm15, [rsp].GUEST_STATE._Xmm15
 	add rsp, SIZEOF GUEST_STATE				; Cleanup stack
-	ret										; Return to GUEST_STATE.Rip
+	ret										
 __vmexit_entry ENDP
 
 END
