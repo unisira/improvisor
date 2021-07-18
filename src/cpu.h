5f149689fcfd89e9c3d83428f36e120b451a67db
unisira <physkotic@gmail.com>
2021-07-19 00:16:27 +0100

    Changed to an 'unaligned' M128 integer type to fix CPU_STATE alignment issue

--- src/cpu.h
+++ src/cpu.h
@@ -1,8 +1,16 @@
 #ifndef IMP_CPU_H
 #define IMP_CPU_H
 
-#include <wdm.h>
+#include <ntdef.h>
 
+// Unaligned 128-bit integer type
+typedef struct _M128
+{
+    UINT64 Low;
+    UINT64 High;
+} M128, *PM128;
+
+#pragma pack(push, 1)
 typedef struct _CPU_STATE
 {
     UINT64 Rax;
@@ -23,24 +31,25 @@ typedef struct _CPU_STATE
     UINT64 Rsp;
     UINT64 Rip;
     UINT32 MxCsr;
-    M128A Xmm0;
-    M128A Xmm1;
-    M128A Xmm2;
-    M128A Xmm3;
-    M128A Xmm4;
-    M128A Xmm5;
-    M128A Xmm6;
-    M128A Xmm7;
-    M128A Xmm8;
-    M128A Xmm9;
-    M128A Xmm10;
-    M128A Xmm11;
-    M128A Xmm12;
-    M128A Xmm13;
-    M128A Xmm14;
-    M128A Xmm15;
+    M128 Xmm0;
+    M128 Xmm1;
+    M128 Xmm2;
+    M128 Xmm3;
+    M128 Xmm4;
+    M128 Xmm5;
+    M128 Xmm6;
+    M128 Xmm7;
+    M128 Xmm8;
+    M128 Xmm9;
+    M128 Xmm10;
+    M128 Xmm11;
+    M128 Xmm12;
+    M128 Xmm13;
+    M128 Xmm14;
+    M128 Xmm15;
     UINT64 RFlags;
 } CPU_STATE, *PCPU_STATE;
+#pragma pack(pop)
 
 EXTERN_C
 VOID
@@ -54,6 +63,5 @@ VOID
 __cpu_restore_state(
     _In_ PCPU_STATE
 );
-
 
 #endif
