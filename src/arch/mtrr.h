99ac1dfce3c058442865f3cc9ebd73c74b27bd55
unisira <physkotic@gmail.com>
2022-01-27 14:30:31 +0000

    Implement MTRR structures and add prototypes for MTRR cache

---
+++ src/arch/mtrr.h
@@ -0,0 +1,91 @@
+#ifndef IMP_ARCH_MTRR_H
+#define IMP_ARCH_MTRR_H
+
+#include <ntdef.h>
+
+#define IA32_MTRR_CAPABILITIES 0xFE
+#define IA32_MTRR_DEFAULT_TYPE 0x2FF
+#define IA32_MTRR_PHYSBASE_0 0x200
+#define IA32_MTRR_PHYSMASK_0 0x201
+
+typedef union _IA32_MTRR_CAPABILITIES_MSR
+{
+    UINT64 Value;
+
+    struct
+    {
+        UINT64 VariableRangeRegCount : 8;
+        UINT64 FixedRangeRegSupport : 1;
+        UINT64 Reserved1 : 1;
+        UINT64 WriteCombineTypeSupport : 1;
+        UINT64 SmrrInterfaceSupport : 1;
+        UINT64 Reserved2 : 52:
+    };
+} IA32_MTRR_CAPABILITIES_MSR, *PIA32_MTRR_CAPABILITIES_MSR;
+
+typedef union _IA32_MTRR_DEFAULT_TYPE_MSR
+{
+    UINT64 Value;
+
+    struct
+    {
+        UINT64 Type : 8;
+        UINT64 Reserved1 : 2;
+        UINT64 EnableFixedMtrr : 1;
+        UINT64 EnableMtrr : 1;
+        UINT64 Reserved2 : 52;
+    };
+} IA32_MTRR_DEFAULT_TYPE_MSR, *PIA32_MTRR_DEFAULT_TYPE_MSR;
+
+typedef union _IA32_MTRR_PHYSBASE_N_MSR
+{
+    UINT64 Value;
+
+    struct
+    {
+        UINT64 Type : 8;
+        UINT64 Reserved1 : 4;
+        UINT64 Base : 36;
+        UINT64 Reserved2 : 16;
+    };
+} IA32_MTRR_PHYSBASE_N_MSR, *PIA32_MTRR_PHYSBASE_N_MSR;
+
+typedef union _IA32_MTRR_PHYSMASK_N_MSR
+{
+    UINT64 Value;
+
+    struct
+    {
+        UINT64 Reserved1 : 11;
+        UINT64 Valid : 1;
+        UINT64 Mask : 36;
+        UINT64 Reserved2 : 16;
+    };
+} IA32_MTRR_PHYSMASK_N_MSR, *PIA32_MTRR_PHYSMASK_N_MSR;
+
+typedef enum _MEMORY_TYPE
+{
+    MT_UNCACHABLE = 0,
+	MT_WRITE_COMBINING = 1,
+	MT_WRITE_THROUGH = 4,
+	MT_WRITE_PROTECTED = 5,
+	MT_WRITEBACK = 6,
+	MT_UNCACHED = 7,          //<! Table 11-10. Memory Types That Can Be Encoded With PAT
+
+	MT_INVALID = 0xFF
+} MEMORY_TYPE;
+
+// Iterator function type for iterating MTRR ranges
+typedef VOID(*MTRR_RANGE_ITERATOR)(IA32_MTRR_PHYSBASE_N, IA32_MTRR_PHYSMASK_N);
+
+MEMORY_TYPE
+MtrrGetTypeFromPhysAddr(
+    _In_ UINT64 PhysAddr
+);
+
+VOID
+MtrrIterateVariableRanges(
+    _In_ MTRR_RANGE_ITERATOR Iter
+);
+
+#endif
