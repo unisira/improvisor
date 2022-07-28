da4739d1f0ac26db19ef53c911ed3cee1f824005
unisira <physkotic@gmail.com>
2022-07-28 16:06:37 +0100

    Refactor section labels to follow naming convention, add VMM shared data section

--- src/section.h
+++ src/section.h
@@ -7,11 +7,17 @@
 #pragma section(".VSC", read, execute)
 #pragma section(".VMMC", read, execute)
 #pragma section(".VMMD", read, write)
+#pragma section(".VMMSD", read, write)
 #pragma section(".VMMRD", read)
 
 #pragma data_seg(".VMMD$1")
 #pragma data_seg(".VMMD$2")
 #pragma data_seg(".VMMD$3")
+#pragma data_seg()
+
+#pragma data_seg(".VMMSD$1")
+#pragma data_seg(".VMMSD$2")
+#pragma data_seg(".VMMSD$3")
 #pragma data_seg()
 
 #pragma const_seg(".VMMRD$1")
@@ -29,17 +35,20 @@
 #pragma code_seg(".VSC$3")
 #pragma code_seg()
 
-__declspec(allocate(".VMMD$1")) static char VmmDataStart = 0x0;
-__declspec(allocate(".VMMD$3")) static char VmmDataEnd = 0x0;
+__declspec(allocate(".VMMD$1")) static char sVmmDataStart = 0x0;
+__declspec(allocate(".VMMD$3")) static char sVmmDataEnd = 0x0;
 
-__declspec(allocate(".VMMRD$1")) static char VmmRDataStart = 0x0;
-__declspec(allocate(".VMMRD$3")) static char VmmRDataEnd = 0x0;
+__declspec(allocate(".VMMSD$1")) static char sVmmSDataStart = 0x0;
+__declspec(allocate(".VMMSD$3")) static char sVmmSDataEnd = 0x0;
 
-__declspec(allocate(".VMMC$1")) static char VmmTextStart = 0x0;
-__declspec(allocate(".VMMC$3")) static char VmmTextEnd = 0x0;
+__declspec(allocate(".VMMRD$1")) static char sVmmRDataStart = 0x0;
+__declspec(allocate(".VMMRD$3")) static char sVmmRDataEnd = 0x0;
 
-__declspec(allocate(".VSC$1")) static char VscStart = 0x0;
-__declspec(allocate(".VSC$3")) static char VscEnd = 0x0;
+__declspec(allocate(".VMMC$1")) static char sVmmTextStart = 0x0;
+__declspec(allocate(".VMMC$3")) static char sVmmTextEnd = 0x0;
+
+__declspec(allocate(".VSC$1")) static char sVscStart = 0x0;
+__declspec(allocate(".VSC$3")) static char sVscEnd = 0x0;
 
 #define VMM_API \
 	__declspec(code_seg(".VMMC$2")) DECLSPEC_NOINLINE
@@ -48,6 +57,9 @@ __declspec(allocate(".VSC$3")) static char VscEnd = 0x0;
 	__declspec(code_seg(".VSC$2")) DECLSPEC_NOINLINE
 
 #define VMM_DATA \
+	__declspec(allocate(".VMMD$2"))
+
+#define VMM_SDATA \
 	__declspec(allocate(".VMMD$2"))
 
 #define VMM_RDATA \
