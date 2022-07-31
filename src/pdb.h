0716c429a01ffb7ec5a773242bc0866c28791499
unisira <physkotic@gmail.com>
2022-07-31 22:46:58 +0100

    Implement the majority of PDB parsing and offset searching

---
+++ src/pdb.h
@@ -0,0 +1,35 @@
+#ifndef IMP_PDB_H
+#define IMP_PDB_H
+
+#include "util/hash.h"
+
+#include <ntdef.h>
+#include <wdm.h>
+
+NTSTATUS
+PdbReserveEntries(
+	_In_ SIZE_T Count
+);
+
+PVOID
+PdbFindSymbol(
+	_In_ FNV1A Pdb,
+	_In_ FNV1A Name,
+	_In_ UINT64 Flags
+);
+
+SIZE_T
+PdbFindMemberOffset(
+	_In_ FNV1A Pdb,
+	_In_ FNV1A Structure,
+	_In_ FNV1A Member
+);
+
+NTSTATUS
+PdbParseFile(
+	_In_ FNV1A Name,
+	_In_ PVOID ImageBase,
+	_In_ PVOID Pdb
+);
+
+#endif
