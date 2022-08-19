a18ae0780d934d387c9b1f46b7ee7a5435c5d1e8
unisira <physkotic@gmail.com>
2022-08-19 18:38:05 +0100

    Add thread for printing log records

--- src/main.c
+++ src/main.c
@@ -1,7 +1,42 @@
 #include "improvisor.h"
+#include "vmcall.h"
 #include "vmm.h"
 
 BOOLEAN gIsHypervisorRunning;
+
+VOID
+LogThreadEntry(VOID)
+{
+    CHAR Log[512] = { 0 };
+    
+    while (!gIsHypervisorRunning);
+
+    LARGE_INTEGER Time = {
+        .QuadPart = -500000
+    };
+
+#if 1
+    while (TRUE)
+    {
+#if 1
+        // Get one log from the hypervisor and print it
+        HYPERCALL_RESULT HResult = VmGetLogRecords(Log, 1);
+        if (HResult != HRESULT_SUCCESS)
+            continue;
+
+        ImpDebugPrint("LOG: %s", Log);
+#else
+        PIMP_LOG_RECORD Log = NULL;
+        if (!NT_SUCCESS(ImpRetrieveLogRecord(&Log)))
+            continue;
+
+        ImpDebugPrint("%s", Log->Buffer);
+#endif
+
+        KeDelayExecutionThread(KernelMode, FALSE, &Time);
+    }
+#endif
+}
 
 VOID
 DriverUnload(
@@ -26,6 +61,28 @@ DriverEntry(
     // Set our driver unload routine so we can unload the driver for debugging
     DriverObject->DriverUnload = DriverUnload;
 #endif
+
+    OBJECT_ATTRIBUTES ObjectAttributes;
+    InitializeObjectAttributes(&ObjectAttributes,
+        NULL,
+        OBJ_KERNEL_HANDLE,
+        NULL,
+        NULL);
+
+    HANDLE LogThread = NULL;
+    Status = PsCreateSystemThread(
+        &LogThread,
+        THREAD_ALL_ACCESS,
+        &ObjectAttributes,
+        NULL,
+        NULL,
+        (PKSTART_ROUTINE)LogThreadEntry,
+        NULL);
+    if (!NT_SUCCESS(Status))
+    {
+        ImpDebugPrint("Failed to start log thread... (%X)\n", Status);
+        return Status;
+    }
 
     Status = VmmStartHypervisor();
     if (!NT_SUCCESS(Status))
